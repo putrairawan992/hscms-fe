d39553a91e908f2417a8fc9e90ece252503f704b
johan <johan@mail.com>
2025-03-07 16:03:09 +0700

    add agreemnt in talent page

---
+++ components/AgreementModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
+      <v-card-title class="headline">Agreement</v-card-title>
+      <v-card-text>
+        By downloading these files, you agree to the terms and conditions.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click="onAgree">Agree</v-btn>
+        <v-btn color="error" @click="onCancel">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: Boolean,
+  },
+  methods: {
+    onAgree() {
+      this.$emit("agree");
+    },
+    onCancel() {
+      this.$emit("cancel");
+    },
+  },
+};
+</script>
